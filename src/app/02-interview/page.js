251a735d2be08060c944e7bfaaf4b1b2cbdef0b0
portfolioflex <portfolioflex2023@gmail.com>
2024-06-23 21:41:49 -0300

    feat: finish excercise 02

--- src/app/02-interview/page.js
+++ src/app/02-interview/page.js
@@ -2,12 +2,11 @@ import React from 'react';
 
 import { getComments } from '@/helpers/interview-helpers';
 import Comment from '@/components/Comment';
+import Spinner from '@/components/Spinner';
 
 export const dynamic = 'force-dynamic';
 
-async function InterviewExercise() {
-  const comments = await getComments();
-
+function InterviewExercise() {
   return (
     <>
       <article className="main-article">
@@ -124,17 +123,25 @@ async function InterviewExercise() {
         </div>
       </article>
 
-      <section className="comments-section">
-        <h2>Discussion</h2>
-        {comments.map((comment) => (
-          <Comment
-            key={comment.id}
-            comment={comment}
-          />
-        ))}
-      </section>
+        <section className="comments-section">
+          <h2>Discussion</h2>
+          <React.Suspense fallback={<Spinner />}>
+            <Comments />
+          </React.Suspense>
+        </section>
     </>
   );
+}
+
+async function Comments() {
+  const comments = await getComments();
+
+  return comments.map((comment) => (
+    <Comment
+      key={comment.id}
+      comment={comment}
+    />
+  ))
 }
 
 export default InterviewExercise;
